8487824ee0335fdb5d0309928e0501cdfa2d6c22
dyzun <dyzun71@gmail.com>
2016-12-04 16:48:20 -0500

    javadocs and new method

--- GroupProject/src/edu/uga/cs4300/logiclayer/LogicImpl.java
+++ GroupProject/src/edu/uga/cs4300/logiclayer/LogicImpl.java
@@ -12,6 +12,10 @@ import edu.uga.cs4300.objectlayer.Review;
 import edu.uga.cs4300.objectlayer.User;
 import edu.uga.cs4300.persistlayer.PersistImpl;
 
+/**
+ *
+ * @author Owner
+ */
 public class LogicImpl {
 
 	HttpServletRequest request=null;
@@ -21,14 +25,23 @@ public class LogicImpl {
 	ArrayList<Review> reviewList = new ArrayList<>();
 	ArrayList<User> userList = new ArrayList<>();
 
-	public LogicImpl(HttpServletRequest req, HttpServletResponse res){
+    /**
+     *
+     * @param req
+     * @param res
+     */
+    public LogicImpl(HttpServletRequest req, HttpServletResponse res){
 		request = req;
 		response= res;
 		persist = new PersistImpl();
 	} //constructor
         
-        
-        public void checkLoginInfo(String username, String password){
+    /**
+     * Takes info to get the current user, if either wrong, sql throws exception
+     * @param username 
+     * @param password
+     */
+    public void checkLoginInfo(String username, String password){
             ResultSet info = persist.getUserForSignIn(username, password);
             
             try{
@@ -38,14 +51,17 @@ public class LogicImpl {
                     }
             }
              catch (SQLException e) {
-			// TODO Auto-generated catch block
+			// TODO REPLACE WITH ERROR MESSAGE/VARIBLE
 			e.printStackTrace();
 		}
             
         }//checkLoginInfo
         
-        
-	public ArrayList<Game> getAllGames(){
+    /**
+     * used to generate list of all games
+     * @return full list of all games
+     */
+    public ArrayList<Game> getAllGames(){
 		//get the result sets
 		ResultSet games = persist.getGames();
 
@@ -70,7 +86,12 @@ public class LogicImpl {
 		return gameList;
 	}//getAllGames
 
-	public ArrayList<Game> getGamesByGenre(String genre){
+    /**
+     * used to generate list of all games of a given genre
+     * @param genre
+     * @return list of games in genre
+     */
+    public ArrayList<Game> getGamesByGenre(String genre){
 		//get the result sets
 		ResultSet games = persist.getGames();
 
@@ -104,7 +125,12 @@ public class LogicImpl {
 		return gameList;
 	}//getGamesByGenre
 
-	public ArrayList<Game> getGamesByConsole(String console){
+    /**
+     * used to generate list of all games of a given console
+     * @param console
+     * @return list of all games on said console
+     */
+    public ArrayList<Game> getGamesByConsole(String console){
 		//get the result sets
 		ResultSet games = persist.getGames();
 
@@ -138,7 +164,11 @@ public class LogicImpl {
 		return gameList;
 	}//getGamesByConsole
 
-	public ArrayList<User> getUser(){
+    /**
+     *
+     * @return username and id
+     */
+    public ArrayList<User> getUser(){
 		ResultSet users = persist.getUser();
 
 		try {
@@ -156,12 +186,27 @@ public class LogicImpl {
 		return userList;
 	}//getUser
         
-        public void addUser(String username,String email,String password, String address,
+    /**
+     * used to import user into table
+     * @param username
+     * @param email
+     * @param password
+     * @param address
+     * @param city
+     * @param zip_code
+     * @param state
+     */
+    public void addUser(String username,String email,String password, String address,
                 String city, int zip_code, String state){
             persist.addUser(username,email,password,address,city,zip_code,state);
         }//addUser
 
-	public ArrayList<Review> getReviewsByGame(int game_id){
+    /**
+     * used to general list of all reviews for a game
+     * @param game_id
+     * @return list of reviews per game with @param
+     */
+    public ArrayList<Review> getReviewsByGame(int game_id){
 		//get the result sets
 		ResultSet reviews = persist.getReviews(game_id);
 		ResultSet users = persist.getUser();			
@@ -191,11 +236,23 @@ public class LogicImpl {
 		return reviewList;
 	}//getReviewsByGame
         
-        public void addReview(int game_id, String review,int user_id,int score){
+    /**
+     * Used to add review to table
+     * @param game_id
+     * @param review
+     * @param user_id
+     * @param score
+     */
+    public void addReview(int game_id, String review,int user_id,int score){
             persist.addReview(game_id, review, user_id, score);
         }//addReview
 
-	public ArrayList<Game> getCart(int user_id){
+    /**
+     * used to return cart based on user
+     * @param user_id
+     * @return a list of all games in a users cart
+     */
+    public ArrayList<Game> getCart(int user_id){
 		//get the result sets
 		ResultSet cart = persist.getCart(user_id);			
 		try{
@@ -225,5 +282,14 @@ public class LogicImpl {
 		}//trycatch
 		return gameList;
 	}//getCart
+        
+    /**
+     * used to add game per cart
+     * @param user_id
+     * @param game_id
+     */
+    public void addToCart(int user_id, int game_id){
+            persist.addToCart(user_id, game_id);
+        }
 
 }
--- GroupProject/src/edu/uga/cs4300/persistlayer/PersistImpl.java
+++ GroupProject/src/edu/uga/cs4300/persistlayer/PersistImpl.java
@@ -3,31 +3,57 @@ package edu.uga.cs4300.persistlayer;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
-
+/**
+ *
+ * @author Owner
+ */
 public class PersistImpl {
 	//open connection
 	DbAccessImpl access= new DbAccessImpl();
 	Connection con = access.connect();
 	
-	public ResultSet getGames(){
+    /**
+     * used in LogicImpl for getting all games
+     * @return all games
+     */
+    public ResultSet getGames(){
 		String games = "SELECT * FROM games";
 		ResultSet rs = access.retrieve(con, games);
 		return rs;
 	}//getGames
 	
-	public ResultSet getGamesById(int game_id){
+    /**
+     * get a specific game based on its ID
+     * @param game_id
+     * @return single row from SQL table of game with id
+     */
+    public ResultSet getGamesById(int game_id){
 		String games = "SELECT * FROM games WHERE id =" + game_id;
 		ResultSet rs = access.retrieve(con, games);
 		return rs;
 	}//getGamesbyId
 	
-	public ResultSet getUser(){
+    /**
+     * used to get all users
+     * @return list of all user info
+     */
+    public ResultSet getUser(){
 		String user = "SELECT * FROM user";
 		ResultSet rs = access.retrieve(con,user);
 		return rs;
 	}//getUser
         
-        public void addUser(String username,String email,String password, String address,
+    /**
+     * used to add user into table
+     * @param username
+     * @param email
+     * @param password
+     * @param address
+     * @param city
+     * @param zip_code
+     * @param state
+     */
+    public void addUser(String username,String email,String password, String address,
                 String city, int zip_code, String state){
             String user = "INSERT INTO `project_group`.`user`(`username`,`email`,"
                     + "`password`,`address`,`city`,`zip_code`,`state`) VALUES ("
@@ -36,44 +62,92 @@ public class PersistImpl {
             int update = access.update(con,user);
         }
 	
-	public ResultSet getUserById(int user_id){
+    /**
+     * used to get specific user
+     * @param user_id
+     * @return return single row of user table based on user_id
+     */
+    public ResultSet getUserById(int user_id){
 		String user = "SELECT * FROM user WHERE id = " + user_id;
 		ResultSet rs = access.retrieve(con,user);
 		return rs;
 	}//getUser
         
-        public ResultSet getUserForSignIn(String user, String pass){
-                String query = "SELECT 1 FROM user WHERE username = '"+user+"' AND password = '"+pass+"'";
+    /**
+     * used to check login info
+     * @param user
+     * @param pass
+     * @return single row of user with password, meant to fail if either wrong
+     */
+    public ResultSet getUserForSignIn(String user, String pass){
+                String query = "SELECT * FROM user WHERE username = '"+user+"' AND password = '"+pass+"'";
                 ResultSet rs = access.retrieve(con, query);
                 return rs;
         }//getUser for login
         
-	
-	public ResultSet getCart(int user_id){
+    /**
+     * used to return all items in a users cart
+     * @param user_id
+     * @return all rows in cart table based on user
+     */
+    public ResultSet getCart(int user_id){
 		String cart = "SELECT * FROM cart WHERE user_id = " +user_id;
 		ResultSet rs = access.retrieve(con, cart);
 		return rs;
 	}//getCart
+        
+    /**
+     * used to add item to cart based on user id and game id
+     * @param user_id
+     * @param game_id
+     */
+    public void addToCart(int user_id, int game_id){
+		String cart = "INSERT INTO `project_group`.`cart`(`user_id`,`game_id`)"
+                        + "VALUES ( "+user_id+","+game_id+")";
+		int update = access.update(con,cart);
+	}//getCart
 	
-	public ResultSet getGenre(int game_id){
+    /**
+     * used to get genre of a selected game
+     * @param game_id
+     * @return all genres of a selected game
+     */
+    public ResultSet getGenre(int game_id){
 		String genre = "SELECT * FROM game_genre WHERE game_id = " +game_id;
 		ResultSet rs = access.retrieve(con, genre);
 		return rs;
 	}//getGenre
 	
-	public ResultSet getConsoles(int game_id){
+    /**
+     * used to get console of a selected game
+     * @param game_id
+     * @return all consoles of a selected game
+     */
+    public ResultSet getConsoles(int game_id){
 		String console = "SELECT * FROM game_consule WHERE game_id = " +game_id;
 		ResultSet rs = access.retrieve(con,console);
 		return rs;
 	}//getConsoles
 	
-	public ResultSet getReviews(int game_id){
+    /**
+     * used to return all reviews for a selected game
+     * @param game_id
+     * @return all reviews based on game id
+     */
+    public ResultSet getReviews(int game_id){
 		String reviews = "SELECT * FROM game_reviews WHERE game_id = " +game_id;
 		ResultSet rs = access.retrieve(con,reviews);
 		return rs;
 	}//getReviews
 	
-	public void addReview(int game_id,String review,int user_id,int score){
+    /**
+     * used to add review to table
+     * @param game_id
+     * @param review
+     * @param user_id
+     * @param score
+     */
+    public void addReview(int game_id,String review,int user_id,int score){
 		String reviews = "INSERT INTO `project_group`.`game_reviews`(`game_id`,`review`,"
 				+ "`user_id`,`score`) VALUES ("+game_id+",'"+review+"',"
 				+user_id+","+score+");";
